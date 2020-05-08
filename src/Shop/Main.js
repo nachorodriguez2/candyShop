40b13cda44fc0010140d09473d686145d6e967c3
nachorodriguez2 <nacho_rodriguez85@gmail.com>
2020-05-08 20:05:15 -0300

    cambios 3

--- src/Shop/Main.js
+++ src/Shop/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
       <div className="texto">
         <i className="fas fa-candy-cane"></i>
         <h1>
-          Bienvenido a la tienda <br /> virtual de CandyMarket
+          Bienvenido a la tiendaxxx <br /> virtual de CandyMarket
         </h1>
         <p>En la seccion 'SHOP' encontrará nuestros productos seleccionados</p>
       </div>
